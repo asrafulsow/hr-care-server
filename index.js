c825b59b76297eeeedc2553ef3c80f22e119347a
asrafulsow <aisowrav11@gmail.com>
2022-02-08 17:58:29 +0600

    mongodb-setup

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+//middelweare
+app.use(cors())
+app.use(express.json())
+
+//mongoDB connection here
+
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("insertDB");
+    const userCollection = database.collection("user");
+    // create a document to insert
+    
+    // get method
+
+
+
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
